7bfd268644ff82bbe5526c8a7d0630dede51c892
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2025-12-14 21:04:45 -0600

    Add facts.json

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-var i = 0;
-const fact = "I want you";
-var speed = 50;
-function typeWriter() {
-  if (i < fact.length) {
-    document.getElementById("fact").innerHTML += fact.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+async function fetchFacts() {
+  const response = await fetch("src/data/facts.json");
+  const data = await response.json();
+  return data.facts;
 }
 
-typeWriter();
+function displayFact(fact, factParagraph) {
+  factParagraph.innerHTML = ""; // Clear the element first
+
+  function typeWriter(i = 0) {
+    const speed = 50;
+    if (i < fact.length) {
+      factParagraph.innerHTML += fact.charAt(i);
+      setTimeout(() => typeWriter(i + 1), speed);
+    }
+  }
+  typeWriter();
+}
+
+async function main() {
+  const facts = await fetchFacts();
+  console.log("facts", facts);
+
+  const factParagraph = document.getElementById("fact");
+  const nextFactBtn = document.getElementById("nextFactBtn");
+
+  let currentIndex = Math.floor(Math.random() * facts.length);
+
+  // Display the first fact
+  displayFact(facts[currentIndex].fact, factParagraph);
+
+  // Add click event listener to the button
+  nextFactBtn.addEventListener("click", () => {
+    window.location.reload();
+  });
+}
+
+main();
